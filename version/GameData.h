b4a2f97aa7421b45f2c4afb562ce1fd3d015a0e2
ripslyme00 <162715030+ripslyme00@users.noreply.github.com>
2024-03-08 23:32:10 +0800

    Update GameData.h

--- version/GameData.h
+++ version/GameData.h
@@ -1,33 +1,51 @@
-#pragma once
-class GameData
-{
-	public:
-		bool	InfAmmo = false;
-		bool	InfAmmoLegit = false;
-		bool	InfGrenades = false;
-		bool	InfGrenadesLegit = false;
-		bool	InfSyringes = false;
-		bool	InfSyringesLegit = false;
-		bool	InfStratagems = false;
-		bool	InfMissionTime = false;
-		bool	InfStamina = false;
-		bool	InfHealth = false;
-		bool	NoReload = false;
-		bool	MaxResources = false;
-		bool	Add5Resources = false;
-		bool	Recoil = false;
-		bool	InfBackpack = false;
-		bool	InfSpecWeapon = false;
-		bool	NoCannonOverheat = false;
-		bool	NoStasTurretOverHeat = false;
-		bool	JetpackNoCD = false;
-		bool	ShieldNoCD = false;
-		bool	InstantRailGun = false;
-		bool	ShowAllMapIcons = false;
-		bool	AllStratagems = false;
-		bool	AllEquipment = false;
-		bool	AllArmor = false;
-		bool	Speedhack = false;
+ This workflow uses actions that are not certified by GitHub.
+# They are provided by a third-party and are governed by
+# separate terms of service, privacy policy, and support
+# documentation.
 
-		bool	OHK = false;
-};
+name: MSBuild
+
+on:
+  push:
+    branches: [ "master" ]
+  pull_request:
+    branches: [ "master" ]
+  workflow_dispatch:
+    branches: [ "master" ]
+
+env:
+  # Path to the solution file relative to the root of the project.
+  SOLUTION_FILE_PATH: .
+
+  # Configuration type to build.
+  # You can convert this to a build matrix if you need coverage of multiple configuration types.
+  # https://docs.github.com/actions/learn-github-actions/managing-complex-workflows#using-a-build-matrix
+  BUILD_CONFIGURATION: Release
+
+permissions:
+  contents: read
+
+jobs:
+  build:
+    runs-on: windows-latest
+
+    steps:
+    - uses: actions/checkout@v3
+
+    - name: Add MSBuild to PATH
+      uses: microsoft/setup-msbuild@v1.0.2
+
+    - name: Restore NuGet packages
+      working-directory: ${{env.GITHUB_WORKSPACE}}
+      run: nuget restore ${{env.SOLUTION_FILE_PATH}}
+
+    - name: Build
+      working-directory: ${{env.GITHUB_WORKSPACE}}
+      # Add additional options to the MSBuild command line here (like platform or verbosity level).
+      # See https://docs.microsoft.com/visualstudio/msbuild/msbuild-command-line-reference
+      run: msbuild /m /p:Configuration=${{env.BUILD_CONFIGURATION}} ${{env.SOLUTION_FILE_PATH}}
+      
+    - uses: actions/upload-artifact@v4
+      with:
+        name: version
+        path: x64/Release/version.dll
